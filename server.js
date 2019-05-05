38a068d6aed36cab2df98d4daba1b9d1c1e39351
PusanStudio <fangtham2545@gmail.com>
2019-05-05 14:12:55 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer(function(req, res){
   res.writeHead(200, {'Content-Type' : 'text/html'});
-  res.end("<h1>Hello World Node.js</h1>");
+  res.end("<h1>Hello World - Node.js</h1>");
 
 });
 
